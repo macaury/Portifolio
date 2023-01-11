336b8f40e03a2777d5e1e4ef0c682a3dca48b671
macaury carvalho <carvalhomacaury@gmail.com>
2023-01-11 19:06:46 -0300

    Update projetos.jsx

--- Portifolio/src/components/projetos.jsx
+++ Portifolio/src/components/projetos.jsx
@@ -28,68 +28,71 @@ function projetos() {
                 Projetos
               </span>
 
+              <Card className="card_sty">
+                <Card.Body>
+                  <Card.Title className="mb-2">QuiZztech</Card.Title>
+                  <Card.Text>
+                    Quiz sobre T.I. Possui tres niveis de dificuldade. Feito em
+                    ReactJS,html e bootstrap.
+                  </Card.Text>
+                </Card.Body>
+
+                <Button
+                  className="mb-3"
+                  variant="dark"
+                  href="https://pedrocslucas.github.io/quizz-tech-site/"
+                  target="_blank"
+                >
+                  Visualizar Projeto
+                </Button>
+                <Button
+                  className="mb-3"
+                  variant="primary"
+                  href="https://github.com/macaury/quiz-tech/tree/master"
+                  target="_blank"
+                >
+                  Ver no GitHub
+                </Button>
+              </Card>
+
               <Card className="card_sty ">
                 <Card.Body>
-                  <Card.Title>Karowara</Card.Title>
+                  <Card.Title className="mb-2">Karowara</Card.Title>
                   <Card.Text className="text_card">
                     <span>
                       software criado para auxiliar os professores no ensino de
                       noções de algoritmo, e facilitar a visualização de
-                      estruturas abstratas básicas.
+                      estruturas abstratas básicas. Desenvolvido totalmente em
+                      Java.
                     </span>
-                    <Row className="text-center">
-                      <span style={{ color: "red" }}>Desenvolvido em :</span>
-                      <span
-                        style={{
-                          border: "solid 2px red",
-                          borderRadius: "50px",
-                          width: "70px",
-                          marginTop: "5px",
-                          marginLeft: "40%",
-                        }}
-                      >
-                        Java
-                      </span>
-                    </Row>
                   </Card.Text>
                 </Card.Body>
-                <Button className="mb-3" variant="primary">
-                  Ver no <span style={{color: "black"}}>GitHub</span>
-                </Button>
                 <Button className="mb-3" variant="dark">
-                  Preview
+                  Visualizar Projeto
+                </Button>
+                <Button className="mb-3" variant="primary">
+                  Ver no <span style={{ color: "black" }}>GitHub</span>
                 </Button>
               </Card>
 
               <Card className="card_sty">
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title className="mb-2">Card Title</Card.Title>
                   <Card.Text>
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </Card.Text>
                 </Card.Body>
-                <Button className="mb-3" variant="primary">
-                  Ver no GitHub
-                </Button>
                 <Button className="mb-3" variant="dark">
-                  Preview
+                  Visualizar Projeto
                 </Button>
-              </Card>
-
-              <Card className="card_sty">
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                </Card.Body>
-                <Button className="mb-3" variant="primary">
+                <Button
+                  className="mb-3"
+                  variant="primary"
+                  href="https://github.com/macaury/ranking_movies#Como-rodar-o-projeto-na-sua-maquina"
+                  target="_blank"
+                >
                   Ver no GitHub
-                </Button>
-                <Button className="mb-3" variant="dark">
-                  Preview
                 </Button>
               </Card>
             </Row>
