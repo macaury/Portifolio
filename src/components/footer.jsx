d1ea7c07b2ce20de5463c72129e0f92d9daed4ac
macaury carvalho <carvalhomacaury@gmail.com>
2023-01-20 19:05:51 -0300

    update build

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 
-import tree_oak  from "../assets/images/tree_oak_footer.svg";
+import tree_oak  from "../icons/tree_oak_footer.svg";
 
 function footer() {
   return (
