6b87f88f9442a21075c52fc817b43a8de6c0ea95
macaury carvalho <carvalhomacaury@gmail.com>
2023-01-25 18:02:13 -0300

    update

--- src/components/sobre_mim.jsx
+++ src/components/sobre_mim.jsx
@@ -3,16 +3,15 @@ import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import img from "./icons/img.png";
 
 function quemSou() {
   return (
     <>
-      <div id="sobre_mim" className="bg_projetos">
+      <div id="sobre_mim" className="bg_projetos justify-content-center">
         <Container>
           <Row style={{ paddingTop: "2%" }}>
             <span
-              className="titulo text-center justify-content-center "
+              className="titulo text-center  "
               style={{
                 marginBottom: "30px",
               }}
@@ -20,21 +19,17 @@ function quemSou() {
               Quem sou
             </span>
           </Row>
-          <Row className="d-flex flex-row ">
-            <Col xs md="7" sm="6">
-              <div className="profile_pict">
-                <img
-                  src={img}
-                  alt="macaury_carvalho"
-                 
-                />
-              </div>
+          <Row className="d-flex flex-row  ">
+            <Col xs md="7" sm="5">
+              <div className="profile_pict align-items-center"></div>
             </Col>
-            <Col xs md="5" sm="6">
+            <Col xs md="5" sm="7">
               <p>
-                Meu nome é Macaury Carvalho.Sou amante de café, design simples e ouvinte de mpb.Atualmente trabalho como
-                freenlancer de desenvolvimento front-end e Ui design.
-                Desenvolvo interfaces modernas e de alta qualidade, concentrado em performance e responsividade.
+                Meu nome é Macaury Carvalho.Sou amante de café, design simples e
+                ouvinte de mpb.Atualmente trabalho como freenlancer de
+                desenvolvimento front-end e Ui design. Desenvolvo interfaces
+                modernas e de alta qualidade, concentrado em performance e
+                responsividade.
               </p>
             </Col>
           </Row>
